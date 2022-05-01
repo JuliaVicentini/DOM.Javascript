6d493107bdad9688056005dd1c3ab7cf7a9b7a37
JuliaVicentini <doamaralju@gmail.com>
2022-05-01 10:50:07 -0300

    Arrumei o botão edit

--- lista.js
+++ lista.js
@@ -1,3 +1,31 @@
+function DeleteItem(){
+    this.parentElement.remove()
+}
+
+function EditItem(){
+    item_lista = this.parentElement
+    texto_item_lista  = item_lista.innerText.replace("DeleteEdit", "")
+    input_element = document.querySelector('[data-form-input]')
+    input_element.value = texto_item_lista
+    
+    const botao_submit = document.querySelector('[button-submit]')
+    botao_submit.className = 'd-none'
+
+    const botao_edit = document.querySelector('[button-edit]')
+    botao_edit.classList.remove('d-none')
+
+}
+
+function CriarBotaoEdit(){
+    const botao_edit =  document.createElement('button')
+    botao_edit.classList.add("btn", "btn-warning")
+    botao_edit.innerHTML = "Edit"
+    botao_edit.type = "button"
+    botao_edit.addEventListener("click", EditItem);
+
+    return botao_edit
+}
+
 function CriarBotaoDelete(){
     const botao_delete =  document.createElement('button')
     botao_delete.classList.add("btn", "btn-danger")
@@ -7,48 +35,6 @@ function CriarBotaoDelete(){
 
     return botao_delete
 }
-function DeleteItem(){
-    console.log(this.parentElement)
-    this.parentElement.remove()
-}
-
-
-function CriarBotaoEdit(){
-    const botao_edit =  document.createElement('button')
-    botao_edit.classList.add("btn", "btn-success")
-    botao_edit.innerHTML = "Edit"
-    botao_edit.type = "button"
-    botao_edit.addEventListener("click", EditItem);
-
-    return botao_edit
-} 
-function EditItem(){
-    const novo_item_lista = document.createElement('input');
-    const FatherElement = this.parentElement
-    FatherElement.innerHTML = '';
-    FatherElement.appendChild(novo_item_lista)
-    FatherElement.appendChild(CriarBotaoEdit())
-    FatherElement.appendChild(CriarBotaoSalvar())
-}
-
-
-function CriarBotaoSalvar(){
-    const botao_salvar =  document.createElement('button')
-    botao_salvar.classList.add("btn", "btn-secondary")
-    botao_salvar.innerHTML = "Salvar"
-    botao_salvar.type = "button"
-    botao_salvar.addEventListener("click", SalvarItem);
-
-    return botao_salvar
-} 
-function SalvarItem(){
-    const input = this.parentElement.children[0] 
-    lista = this.parentElement
-    this.parentElement.innerHTML = input.value
-    lista.appendChild(CriarBotaoDelete())
-    lista.appendChild(CriarBotaoEdit())   
-}
-
 
 function Submit(){
     const lista = document.querySelector('[data-task]')
@@ -65,3 +51,58 @@ function Submit(){
 
     document.getElementById("item").value = ""
 }
+
+function Edit(){
+    
+    texto_adicionado = input_element.value
+    item_lista.innerHTML = texto_adicionado
+
+    item_lista.appendChild(CriarBotaoDelete())
+    item_lista.appendChild(CriarBotaoEdit())
+
+    const botao_submit = document.querySelector('[button-submit]')
+    botao_submit.classList.remove('d-none')
+    botao_submit.classList.add('btn', 'btn-dark')
+
+    const botao_edit = document.querySelector('[button-edit]')
+    botao_edit.classList.add('d-none')
+
+    input_element.value = '';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction(){
+//     console.log("teste inicial");
+// }
+
+// function addItem(){
+//     const inputItem = document.querySelector('[data-form-input]')
+//     const valorItem = inputItem.value
+
+//     const listaDeItems = document.querySelector('[data-task]')
+
+//     novaLabel = document.createElement('label')
+//     novaLabel.innerText = valorItem
+    
+//     novoItem = document.createElement('li')
+
+//     // novoItem.appendChild(criarBotaoConcluir())
+//     novoItem.appendChild(novaLabel)
+//     // novoItem.appendChild(criarBotaoDelete())
+
+//     listaDeItems.appendChild(novoItem)
+
+//     inputItem.value = ""
+//}
